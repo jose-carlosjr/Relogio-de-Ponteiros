44b84a3d54ce82470827a3193ff94bfaa5be7386
Jose <josecarlos099916@gmail.com>
2024-01-16 20:11:33 -0300

    Criei a animação do botão de segundo e minuto
    Através do Javascript

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const hourHand = document.querySelector('.hour-hand')
+const minuteHand = document.querySelector('.minute-hand')
+const secondHand = document.querySelector('.second-hand')
+
+const getTime = () => {
+    const date = new Date()
+
+    return {
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds()
+    }
+}
+
+getTime()
+
+// 1000 EQUIVALE A 1 SEGUNDO
+setInterval(() => {
+    const {seconds} = getTime() // DESTRUCTURING
+    secondHand.style.transform = `translate(0, -50%) rotate(${seconds * 6}deg)`
+}, 1000)
+
+setInterval(() => {
+    const {minutes} = getTime()
+    minuteHand.style.transform = `translate(0, -50%), rotate${minutes}deg`
+}, 1000 * 60)
