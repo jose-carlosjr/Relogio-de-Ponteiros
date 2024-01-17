39f9b6a3936a8cada677de9949c907b5b489d196
Jose <josecarlos099916@gmail.com>
2024-01-17 20:13:51 -0300

    Consertando o ponteiro das horas

--- js/script.js
+++ js/script.js
@@ -14,13 +14,25 @@ const getTime = () => {
 
 getTime()
 
+const {minutes, hours} = getTime()
+let horaAtual = hours * 30
+
+setInterval(() => {
+    const {seconds, minutes, hours} = getTime()
+    horaAtual += 2
+    hourHand.style.transform = `translate(0, -50%) rotate(${horaAtual}deg)`
+}, minutes * 120000)
+
 // 1000 EQUIVALE A 1 SEGUNDO
 setInterval(() => {
     const {seconds, minutes, hours} = getTime() // DESTRUCTURING
     secondHand.style.transform = `translate(0, -50%) rotate(${seconds * 6}deg)`
     minuteHand.style.transform = `translate(0, -50%) rotate(${minutes * 6}deg)`
-    hourHand.style.transform = `translate(0, -50%) rotate(${hours * 30}deg)`
+    hourHand.style.transform = `translate(0, -50%) rotate(${hours * 30}deg)` 
 }, 1000)
+
+
+
 
 /*
     3600 segundos
